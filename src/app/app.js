b2b84da41034a3e013bbeb54aa271e208ba5ebe1
Brandon Arnold <br@ndonarnold.email>
2017-07-21 18:06:30 -0500

    Added ui-router

--- src/app/app.js
+++ src/app/app.js
@@ -1,21 +1,29 @@
 import angular from 'angular';
 import thunkMiddleware from 'redux-thunk';
 import 'ng-redux';
+import 'angular-ui-router';
+import './app.css';
 import { helloWorlds } from '../reducers';
 
-angular.module('app', ['ngRedux'])
+angular.module('app', ['ngRedux', 'ui.router'])
        .config(config);
 
-config.$inject = ['$ngReduxProvider'];
+config.$inject = ['$ngReduxProvider', '$locationProvider', '$stateProvider'];
 
-function config($ngReduxProvider) {
+function config($ngReduxProvider, $locationProvider, $stateProvider) {
   $ngReduxProvider.createStoreWith(
     { helloWorlds },
     [ thunkMiddleware ]
   );
+  
+  $locationProvider.html5Mode({
+    enabled: false,
+    requireBase: false
+  });
+
+  $locationProvider.hashPrefix('!');
 }
 
-require('./app.ng1.controller.js');
-require('./app.ng1.directive.js');
+require('./home/home.ng1.js');
 require('./home/home.ng1.directive.js');
 require('./home/home.ng1.controller.js');
---
+++ src/app/home/home.ng1.js
@@ -0,0 +1,15 @@
+import angular from 'angular';
+
+angular
+  .module('app')
+  .config(['$stateProvider', '$urlRouterProvider', function($stateProvider, $urlRouterProvider) {
+    $urlRouterProvider.when('', '/');
+    $stateProvider.state('home', {
+      abstract: true,
+      template: '<ui-view />',
+    }).state('home.home', {
+      url: '/',
+      template: require('./home.ng1.html'),
+      controller: 'HomeCtrl as home',
+    });
+  }]);
